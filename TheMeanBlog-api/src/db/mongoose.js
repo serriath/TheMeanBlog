cd70f9f995408b082d0add0cd1732bd504dca6a5
William Francis <dev.williamfrancis@gmail.com>
2021-04-12 19:58:45 +0200

    some small cleanup

--- TheMeanBlog-api/src/db/mongoose.js
+++ TheMeanBlog-api/src/db/mongoose.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+import { connect } from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/TheMeanBlog", {
+connect("mongodb://localhost:27017/TheMeanBlog", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
